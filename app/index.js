877c95ed90c07becedc2bc89e97ed66793954d28
kash <kareem.elshahawe@paycelerate.com>
2017-10-23 18:25:17 +0800

    app login WIP

--- app/index.js
+++ app/index.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const hbs = require('express-hbs');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'hbs');
+//Setting up views & template engine
+app.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/views/layouts/layout.hbs',
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World')
-})
+  res.render('index');
+});
 
 app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
