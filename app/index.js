80559efb5de565e73006ede7ec1a71453c69ee5a
kareem <kareem.elshahawe@gmail.com>
2017-10-23 21:38:25 +0800

    Fix handlebars templating
     --Handlebars didn't inject layout as expected,
       turned out that I am using the wrong package.

--- app/index.js
+++ app/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const hbs = require('express-hbs');
+const hbs = require('express-handlebars');
 const app = express();
 const path = require('path');
 
-app.set('view engine', 'hbs');
 //Setting up views & template engine
-app.engine('hbs', hbs.express4({
-  defaultLayout: __dirname + '/views/layouts/layout.hbs',
-}));
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+app.engine('handlebars', hbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.render('index');
+    res.render('index');
 });
 
 app.listen(8080, function() {
-  console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!')
 });
