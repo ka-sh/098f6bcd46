34158204ab46d7014a897d8a9de6f26a49d9370c
kash <kareem.elshahawe@paycelerate.com>
2017-10-25 14:45:10 +0800

    Add login and user data

--- app/index.js
+++ app/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const app = express();
 const path = require('path');
+var bodyParser = require('body-parser');
+const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const utils = require('./utils.js');
 
-const User = require('./models/User.js')
+app.use(cookieParser());
 
 
 //Setting up views & template engine
@@ -16,10 +25,94 @@ app.engine('handlebars', hbs({
 }));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render('index');
+//Setting up parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+//
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
+
+
+
+//Routes
+app.get('/', function(req, res) {
+  res.render('login');
+});
+/**
+ * Handling login
+ */
+app.post('/login', function(req, res) {
+  req.checkBody('uname', 'user name is required.!!').notEmpty();
+  req.checkBody('password', 'Password is required.!!').notEmpty();
+  if (req.validationErrors()) {
+    res.render('login', {
+      errors: req.validationErrors()
+    });
+  } else {
+    User.find({
+      where: {
+        uname: req.body.uname
+      }
+    }).then(function(user) {
+      if (isValidPassword(req.body.password, user.get('password'))) {
+        res.render('home');
+      } else {
+        res.render('login', {
+          errors: [{
+            msg: "Invalid Password"
+          }]
+        });
+      }
+    });
+  }
+});
+
 
 app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
 });
+
+function isValidPassword(passwordText, hashedPassword) {
+  return utils.compareHash(passwordText, hashedPassword);
+}
--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./DB.js');
+const utils = require('../utils.js');
+const uuid = require('uuid');
+
 const User = sequelize.define('user', {
   clientId: {
     type: Sequelize.UUID
@@ -11,5 +14,15 @@ const User = sequelize.define('user', {
     type: Sequelize.STRING
   },
 });
-
+User.sync().then(function resolve() {
+  User.findOrCreate({
+    where: {
+      uname: "test"
+    },
+    defaults: {
+      password: utils.hash('test'),
+      clientId: uuid()
+    }
+  });
+})
 module.exports = User;
